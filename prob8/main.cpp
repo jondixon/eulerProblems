ee904335894f14e77131fa302761b1a86b8dd2dc
Jonathan Dixon <jdixon@Jonathans-Air.ksc.nasa.gov>
2015-04-29 12:03:17 -0400

    started problem 8

--- prob8/main.cpp
+++ prob8/main.cpp
@@ -9,8 +9,38 @@ int main( )
 
     fin.open("longNum.txt");
 
+    if( !fin )
+    {
+        cout << "DID NOT OPEN FILE " << endl;
+        return(0);
+    }
+
     string longNum;
 
     fin >> longNum;
 
+
+    long long int maxProduct = 0;
+
+
+    for( int i = 0; i < 1000 - 5; i++ )
+    {
+        long long int currentProduct = 1;
+
+        for( int j = i; j < i + 4; j++ )
+        {
+            cout << "j: " << j << " i: " << i << endl;
+            currentProduct *= (longNum[i] - '0');
+        }
+
+        if( currentProduct > maxProduct )
+        {
+            maxProduct = currentProduct;
+            cout << "NEW MAX: " << maxProduct << endl;
+        }
+    }
+
+    cout << "MAX: " << maxProduct << endl;
+
+    fin.close();
 }
