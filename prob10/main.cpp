0a8ddfda3d4c3fdffb81c6fac04af54650098f2b
Jonathan Dixon <jdixon@Jonathans-Air.zyxel.com>
2015-05-11 16:13:11 -0500

    solved prob 10

--- prob10/main.cpp
+++ prob10/main.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main( )
 {
-    vector<int> primes;
+    vector<unsigned long long> primes;
 
     primes.push_back( 2 );
 
-    unsigned long long int primeSum = 0;
+    unsigned long long  primeSum = 2;
 
 
-    for ( unsigned long long int i = 3; i > 0; i++ )
+    for ( unsigned long long  i = 3; i > 0; i++ )
     {
         bool prime = true;
 
@@ -21,7 +21,7 @@ int main( )
 
         else
         {
-            for ( unsigned long long int j = 0; j < primes.size() - 1; j++ )
+            for ( unsigned long long  j = 0; j < primes.size() - 1; j++ )
             {
                 if( i % primes[j] == 0 )
                 {
@@ -37,17 +37,18 @@ int main( )
             {
                 primes.push_back( i );
                 primeSum += i;
+                cout << i << endl;
             }
 
 
             if( i > 2000000 )
             {
-                primes.pop_back();
                 break;
             }
         }
 
     }
 
-    cout << "SUM: " << primeSum << endl;
+
+    cout << "\nSUM: " << primeSum << endl;
 }
